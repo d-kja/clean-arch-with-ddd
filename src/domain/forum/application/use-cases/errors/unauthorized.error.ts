110f1e447e5d0a3c36824e090dafb2a338df7b7e
d-kja <89911360+d-kja@users.noreply.github.com>
2023-12-08 03:15:03 -0300

    fix: add interface on error classes for future use

--- src/domain/forum/application/use-cases/errors/unauthorized.error.ts
+++ src/domain/forum/application/use-cases/errors/unauthorized.error.ts
@@ -1,4 +1,6 @@
-export class UnauthorizedError extends Error {
+import { UseCaseError } from '@/core/errors/use-case.error'
+
+export class UnauthorizedError extends Error implements UseCaseError {
   constructor() {
     super('Unauthorized.')
   }
